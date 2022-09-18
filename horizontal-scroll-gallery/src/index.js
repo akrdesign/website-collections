8e9a46c02db92590b6da4818ec4ee08bc0c3bca2
akrdesign <akrdesign04@gmail.com>
2022-09-18 17:01:09 +0530

    Final Commit

---
+++ horizontal-scroll-gallery/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HomePage from './page';
+// import './styles/reset.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HomePage />
+  </React.StrictMode>
+);
+
